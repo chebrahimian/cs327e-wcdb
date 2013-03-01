fa6500f3b8169b503912da8b4dadaaeb8b73bef3
Jordan Naumann <jordan.naumann@gmail.com>
2013-03-01 13:16:09 -0600

    fixing parsing of xml files with version tag

--- python/WCDB1.py
+++ python/WCDB1.py
@@ -1,13 +1,16 @@
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as MD
+import re
 
 def importXml ( r ):
-    text = r.read ()
-    xml = ET.fromstring ( text )
+    rawText = r.read ()
+    xml = ET.fromstring ( rawText )
     return xml
 
 def exportXml ( w, xml ):
     text = ET.tostring ( xml )
+    pattern = re.compile(r'\s+')
+    text = re.sub(pattern, '', text) 
     reparsed = MD.parseString ( text )
     w.write ( reparsed.toprettyxml ( indent = "    " ) )
     
