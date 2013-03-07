e5c815e4f6cd6f983476a5ee1e3791f89a12b456
Jordan Naumann <jordan.naumann@gmail.com>
2013-03-06 19:31:01 -0600

    added comments to WCDB1.py, added test randomizer

--- python/WCDB1.py
+++ python/WCDB1.py
@@ -3,11 +3,20 @@ import xml.dom.minidom as MD
 import re
 
 def importXml ( r ):
+    """
+    reads in xml from stdin (a file) and parses it into an ElementTree
+    r is the reader
+    returns an ElementTree
+    """
     rawText = r.read ()
     xml = ET.fromstring ( rawText )
     return xml
 
 def exportXml ( w, xml ):
+    """
+    converts an ElementTree to a string, then outputs a formatted xml document to stdout ( a file )
+    w is the writer
+    """
     rawText = ET.tostring ( xml )
     pattern = re.compile (r'[^\S ]+')
     text = re.sub ( pattern, '', rawText )
@@ -15,5 +24,10 @@ def exportXml ( w, xml ):
     w.write ( reparsed.toprettyxml ( indent = "\t" ) )
     
 def start ( r, w ):
+    """
+    imports an xml document into an ElementTree, then outputs the ElementTree back to a file
+    r is the reader
+    w is the writer
+    """
     xml = importXml ( r )
     exportXml ( w, xml )
