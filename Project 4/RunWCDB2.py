129d1e6e024a414fc3d61cfe399dd20ed3385003
Jordan Naumann <jordan.naumann@gmail.com>
2013-04-03 14:56:40 -0500

    Adding usage guidelines

--- Project 4/RunWCDB2.py
+++ Project 4/RunWCDB2.py
@@ -2,4 +2,6 @@ import sys
 
 from WCDB2 import start
 
+# to run the program, use this command:
+# python RunWCDB2.py < [input file] > [output file] [hostname] [username] [password] [database]
 start ( sys.stdin, sys.stdout, sys.argv )
