317f8b3a172394800b27d9a190842fa7db090854
benz3778 <ben_z3@utexas.edu>
2013-04-03 14:59:10 -0500

    updated unit tests, added comments

--- Project 4/TestWCDB2.py
+++ Project 4/TestWCDB2.py
@@ -25,31 +25,22 @@ class TestWCDB2 (unittest.TestCase) :
     # sqlLogin
     # ---------
 
-    def test_sqlLogin (self) :
+    def test_sqlLogin (self) : # assert that valid sql connection is made
         c = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
         a = sqlLogin(c)
-        self.assert_str(type(c)) == "<type '_mysql.connection'>" 
-            
-    # ---------
-    # sqlClose
-    # ---------
-    
-    def test_sqlClose (self) :
-        c = [z, jtn395, Jz.~MPm1Cy]
-        a = sqlClose(c)
-        self.assert_str(type(c)) == "<type '_mysql.connection'>" 
+        self.assert_str(type(a)) == "<type '_mysql.connection'>"
             
     # ---------
     # sqlQuery
     # ---------
 
-    def test_sqlQuery (self) :
+    def test_sqlQuery (self) : # assert that queries provide valid sql results
         c = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
         s = "create table HumanImpact ( crisisID text, type text, number int );"
         a = sqlLogin(c)
-        self.assert_str(type(c)) == "<type '_mysql.connection'>" 
-        c.query(s)
-        r = c.use_result()
+        self.assert_str(type(a)) == "<type '_mysql.connection'>" 
+        a.query(s)
+        r = a.store_result()
         self.assert_str(type(r)) == "<type '_mysql.result'>"
 
     def test_sqlQuery2 (self) :
@@ -58,11 +49,11 @@ class TestWCDB2 (unittest.TestCase) :
         t = "insert into HumanImpact values ( '123', 'deaths', 12);"
         u = "select number from HumanImpact;"
         a = sqlLogin(c)
-        self.assert_str(type(c)) == "<type '_mysql.connection'>" 
-        c.query(s)
-        c.query(t)
-        c.query(u)
-        r = c.use_result()
+        self.assert_str(type(a)) == "<type '_mysql.connection'>" 
+        a.query(s)
+        a.query(t)
+        a.query(u)
+        r = a.store_result()
         self.assert_str(type(r)) == "<type '_mysql.result'>"
 
     def test_sqlQuery3 (self) :
@@ -71,22 +62,196 @@ class TestWCDB2 (unittest.TestCase) :
         t = "insert into HumanImpact values ( '123', 'deaths', 12);"
         u = "select * from HumanImpact;"
         a = sqlLogin(c)
-        self.assert_str(type(c)) == "<type '_mysql.connection'>" 
-        c.query(s)
-        c.query(t)
-        c.query(u)
-        r = c.use_result()
+        self.assert_str(type(a)) == "<type '_mysql.connection'>" 
+        a.query(s)
+        a.query(t)
+        a.query(u)
+        r = a.store_result()
         self.assert_str(type(r)) == "<type '_mysql.result'>"
 
     # ---------
     # parseArgs
     # ---------
 
-    def test_parseArgs (self) :
-        jj = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
+    def test_parseArgs (self) : # assert that items are taken from tuple and put into list
+        jj = (z, jtn395, Jz.~MPm1Cy, cs327e_jtn395)
         c = parseArgs(jj)
         self.assert_c == [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
-             
+
+    # ------------
+    # createTables
+    # ------------
+    
+    def test_createTables (self): # check that tables are successfully entered (returns result)
+    	c = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
+    	a = sqlLogin(c)
+        createTables(a)
+        r = "show tables"
+    	self.assert_str(type(r)) == "<type '_mysql.result'>"
+    
+    def test_createTables2 (self): # check that query into tables returns result
+    	c = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
+    	s = "select * from Crises;"
+    	a = sqlLogin(c)
+        createTables(a)
+    	a.query(s)
+    	r = a.store_result()
+    	self.assert_str(type(r)) == "<type '_mysql.result'>"
+    
+    def test_createTables3 (self): # check that correct elements are in database table
+    	c = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
+    	s = "select crisisID from Crises;"
+    	a = sqlLogin(c)
+        createTables(a)
+    	a.query(s)
+    	r = a.store_result()
+    	self.assert_str(type(r)) == "<type '_mysql.result'>"
+
+    # ---------
+    # addTestData
+    # ---------
+
+    def test_addTestData (self) : # check that data added is valid sql 
+        a = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
+        k = sqlLogin(a)
+        createTables(k)
+        addTestData(k)
+        u = "select crisisID from HumanImpact;"
+        k.query(u)
+        r = k.store_result()
+        self.assert_str(type(r)) == "<type '_mysql.result'>"
+
+    def test_addTestData2 (self) : # check that individual rows can be selected and are valid sql
+        a = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
+        k = sqlLogin(a)
+        createTables(k)
+        addTestData(k)
+        u = "select crisisID from HumanImpact;"
+        k.query(u)
+        r = k.use_result()
+        t = r.fetch_row(maxrows = 1)
+        self.assert_str(type(t)) == "<type '_mysql.result'>"
+
+    def test_addTestData3 (self) : # check that data added matches desired
+        a = [z, jtn395, Jz.~MPm1Cy, cs327e_jtn395]
+        k = sqlLogin(a)
+        createTables(k)
+        addTestData(k)
+        u = "select crisisID from HumanImpact;"
+        k.query(u)
+        r = k.use_result()
+        t = r.fetch_row(maxrows = 1)
+        self.assert_t[0] == 'CCD'
+
+    # ---------
+    # openTagAtt
+    # ---------
+
+    def test_openTagAtt (self) : # test with string inputs
+        k = "xyz"
+        l = "abc"
+        m = "def"
+        n = openTagAtt (k, l, m)
+        self.assert_n == "<xyz abc='def'>"
+
+    def test_openTagAtt2 (self) : # int inputs
+        k = 123
+        l = 456
+        m = 789
+        n = openTagAtt (k, l, m)
+        self.assert_n == "<123 456='789'>"
+
+    def test_openTagAtt3 (self) : # whitespace input
+        k = ' '
+        l = ' '
+        m = ' '
+        n = openTagAtt (k, l, m)
+        self.assert_n == "<   =' '>"
+
+    # ---------
+    # closeTagAtt
+    # ---------
+
+    def test_openTagAtt (self) : # str
+        k = "xyz"
+        l = "abc"
+        m = "def"
+        n = openTagAtt (k, l, m)
+        self.assert_n == "<xyz abc='def'/>"
+
+    def test_openTagAtt2 (self) : # int
+        k = 123
+        l = 456
+        m = 789
+        n = openTagAtt (k, l, m)
+        self.assert_n == "<123 456='789'/>"
+
+    def test_openTagAtt3 (self) : # whitespace
+        k = ' '
+        l = ' '
+        m = ' '
+        n = openTagAtt (k, l, m)
+        self.assert_n == "<   =' '/>"
+
+    #--------
+    # openTag
+    #--------
+    
+    def test_openTag (self):
+    	s = "example" #start with string
+    	r = openTag(s)
+    	self.assert_(r == "<example>")
+    
+    def test_openTag2 (self):
+    	s = 123 #start with int
+    	r = openTag(s)
+    	self.assert_(r == "<123>")
+    	
+    def test_openTag3 (self):
+    	s = "" # start with empty string
+    	r = openTag(s)
+    	self.assert_(r == "<>")
+    	    	    	       
+    #-------------
+    # openCloseTag
+    #-------------
+    
+    def test_openCloseTag (self):
+    	s = "example" #start with strings
+    	v = "value"
+    	r = openCloseTag(s, v)
+    	self.assert_(r == "<example>value</example>")
+    
+    def test_openCloseTag2 (self):
+    	s = 123 #start with ints
+    	v = 321
+    	r = openCloseTag(s, v)
+    	self.assert_(r == "<123>321</123>")
+    	
+    def test_openCloseTag3 (self):
+    	s = "" # start with empty string and whitespace
+    	v = " "
+    	r = openCloseTag(s, v)
+    	self.assert_(r == "<> </>")
+
+    #--------
+    # closeTag
+    #--------
+    
+    def test_openTag (self):
+    	s = "example" #start with string
+    	r = openTag(s)
+    	self.assert_(r == "</example>")
+    
+    def test_openTag2 (self):
+    	s = 123 #start with int
+    	r = openTag(s)
+    	self.assert_(r == "</123>")
+    	
+    def test_openTag3 (self):
+    	s = "" # start with empty string
+    	r = openTag(s)
+    	self.assert_(r == "</>")
     # ---------
     # importXml
     # ---------
@@ -130,7 +295,7 @@ class TestWCDB2 (unittest.TestCase) :
         exportXml( w, xml )
         self.assert_(w.getvalue() == "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<THU>\n\t<Team/>\n</THU>\n")
 
-    def test_exportXml3 (self): # 
+    def test_exportXml3 (self): # three-layer nested elements
         r = "<THU><Team><Cooly></Cooly></Team></THU>"
         w = StringIO.StringIO()
         xml = ET.fromstring ( r )
