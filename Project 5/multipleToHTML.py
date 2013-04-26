d4cb787c09fff81939b54c11aa22ff2908db429b
Jordan Naumann <jordan.naumann@gmail.com>
2013-04-25 20:40:08 -0500

    changes to sql html creater

--- Project 5/multipleToHTML.py
+++ Project 5/multipleToHTML.py
@@ -1,61 +1,44 @@
-def main():
+"""
+Reads in a directory path, directory name, and output name
+execute as 
+>>> python multipleToHTML.py [input directory] [output file]
+Example:
+>>> python multipleToHTML.py C:\\Users\\Cameron\\Desktop output.html
+"""
 
-	"""
-	Reads in a directory path, directory name, and output name
-	execute as 
-	>>> python multipleToHTML.py pathname filename output name
-	Example:
-	>>> python multipleToHTML.py C:\\Users\\Cameron\\Desktop querySol output.html
-	"""
+import MySQLdb
+import sys
+import os
 
-	import sys
-	import os
-	mypath = os.path.join(sys.argv[1], sys.argv[2])
+def writeToFile(f, w, c):
+    temp = f.readline()
+    querys = f.read().split ( ";" )
+    querys = querys[:-1]
+    for q in querys :
+        cur = c.cursor()
+        cur.execute(q)
+        results = cur.fetchall()
+        if len (results) > 0 :
+            w.write("\n\n<h3>"+temp+"</h3>\n")
+            w.write("\n<table border=1>")
+            for row in results:
+                    w.write("<tr>")
+                    for i in row:
+                        w.write("<td>"+str(i)+"</td>")
+                    w.write("</tr>")
+            w.write("</table>\n\n")
+        cur.close()
 
-	tohtml = os.path.join(sys.argv[1], sys.argv[3])
-
-	w = open(tohtml, "a")
-
-	w.write("<head>\n")
-
-	for file in os.listdir(mypath):
-		if file.endswith(".txt"):
-			with open(os.path.join(mypath,file), "r") as input:
-				w.write(file)
-				writeToFile(input, w)
-				input.close()
-
-	w.write("\n</head>")
-	w.close()
-
-
-
-def writeToFile(query, w):
-
-	import MySQLdb
-
-	temp = query.readline()
-
-	query = query.read()
-
-	conn = MySQLdb.connect('localhost', 'root', 'database', 'test')
-	c = conn.cursor()
-
-	c.execute(query)
-
-	results = c.fetchall()
-
-	c.close()
-
-	w.write("\n\n<h3>"+temp+"</h3>\n")
-	w.write("\n<table border=1>")
-	for row in results:
-			w.write("<tr>")
-			for i in row:
-				w.write("<td>"+str(i)+"</td>")
-			w.write("</tr>")
-
-	w.write("</table>\n\n")
+for n,d,f in os.walk ( sys.argv [ 1 ] ) :
+    with open ( sys.argv[ 2 ], "w" ) as w :
+        w.write("<head>\n")
+        conn = MySQLdb.connect('localhost', 'jordan', 'password', 'wcdb3')
+        for file in f:
+            if (file.endswith(".txt")) :
+                with open(n + "\\" + file, "r") as input:
+                    w.write(file)
+                    writeToFile(input, w, conn)
+        conn.close()
+        w.write("\n</head>")
 
 
-main()
